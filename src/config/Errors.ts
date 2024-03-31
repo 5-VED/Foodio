1a35111676a09db68a7962b66698dd8f2ef286a8
5-VED <parmarved1@gmail.com>
2024-04-01 06:01:11 +1300

    Added HTTP CODES

---
+++ src/config/Errors.ts
@@ -0,0 +1,15 @@
+export const HTTP_CODES = Object.freeze({
+  OK: '200',
+  CREATED: '201', // The request has been fulfilled and a new resource has been created
+  ACCEPTED: '202', // The request has been accepted for processing, but the processing has not been completed
+
+  BAD_REQUEST: '400', // The server cannot process the request due to a client error
+  UNAUTHORIZED: '401', // The client must authenticate itself to get the requested response
+  FORBIDDEN: '403', // The client does not have access rights to the content
+  NOT_FOUND: '404', // The server cannot find the requested resource
+  METHOD_NOT_ALLOWED: '405', // The request method is not supported for the requested resource
+
+  INTERNAL_SERVER_ERROR: '500', // The server has encountered a situation it doesn't know how to handle
+  NOT_IMPLEMENTED: '501', // The server does not support the functionality required to fulfill the request
+  SERVICE_UNAVAILABLE: '503', // The server is currently unavailable
+});
